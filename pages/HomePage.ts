266705e6205c365d3c9e956972b5b68116728537
Lukasz Gniazdowski <gniazdowski.luk@gmail.com>
2026-03-31 17:10:48 +0200

    feat(footer): Add footer visibility and link tests

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,11 +1,32 @@
-import { type Page } from "@playwright/test";
+import { type Locator, type Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 import { URLs } from "./urls";
 
 export class HomePage extends BasePage {
   readonly url = URLs.home;
 
+  readonly footer: Locator;
+  readonly footerNav: Locator;
+  readonly footerContactLink: Locator;
+  readonly footerJaktestowacLink: Locator;
+  readonly footerAiTestersLink: Locator;
+
   constructor(page: Page) {
     super(page);
+    this.footer = page.getByRole("contentinfo");
+    this.footerNav = page.getByRole("navigation", {
+      name: "Footer navigation",
+    });
+    this.footerContactLink = this.footerNav.getByRole("link", {
+      name: "Contact us",
+    });
+    this.footerJaktestowacLink = this.footer.getByRole("link", {
+      name: "jaktestowac.pl",
+      exact: true,
+    });
+    this.footerAiTestersLink = this.footer.getByRole("link", {
+      name: "AI_Testers",
+      exact: true,
+    });
   }
 }
